47e5d859c8d3a434879bae31c0ec30255568fcc4
MarkAR2012 <159020473+MarkAR2012@users.noreply.github.com>
2024-04-26 17:56:11 +0100

    Updated source to be more clean and readable

--- ProjectOrb.asm
+++ ProjectOrb.asm
@@ -1,14 +1,17 @@
 [org 0x7c00]
 [bits 16]
+; Start of code
 jmp Clear
-Begin:
+
+Begin: ; Set cursor to full block and set video mode to 80x25 text
 mov ah, 0x0
 mov al, 0x03
 int 0x10
 mov ah, 0x01
 mov cx, 0x0007
 int 0x10
-Print:
+
+Print: ; Print character from keyboard to the screen, and check if keys up arrow, down arrow, left arrow, right arrow, del, backspace or enter are pressed
 mov ah, 0x0
 int 0x16
 cmp ah, 0x0e
@@ -16,7 +19,7 @@ jz Del
 cmp ah, 0x53
 jz Clear
 cmp ah, 0x4b
-jz Back
+jz Left
 cmp ah, 0x50
 jz Down
 cmp ah, 0x48
@@ -31,12 +34,14 @@ mov ah, 0x0e
 int 0x10
 PrintEnd:
 jmp Print
-Clear:
+
+Clear: ; Clear screen
 mov ah, 0x0
 mov al, 0x03
 int 0x10
 jmp Begin
-Back:
+
+Left: ; Move cursor left unless already at the most left of screen
 mov ah, 0x03
 int 0x10
 cmp dl, 0
@@ -45,7 +50,8 @@ sub dl, 1
 mov ah, 0x02
 int 0x10
 jmp Print
-Del:
+
+Del: ; Remove character to the left of the cursor, then move the cursor to where that character was
 mov ah, 0x03
 int 0x10
 cmp dl, 0
@@ -58,7 +64,8 @@ mov al, ''
 mov cx, 1
 int 0x10
 jmp Print
-Down:
+
+Down: ; Move cursor down unless already at bottom of screen
 mov ah, 0x03
 int 0x10
 cmp dh, 24
@@ -67,7 +74,8 @@ add dh, 1
 mov ah, 0x02
 int 0x10
 jmp Print
-Up:
+
+Up: ; Move cursor up unless already at top of screen
 mov ah, 0x03
 int 0x10
 cmp dh, 0
@@ -76,7 +84,8 @@ sub dh, 1
 mov ah, 0x02
 int 0x10
 jmp Print
-Right:
+
+Right: ; Move cursor right unless already at top of screen
 mov ah, 0x03
 int 0x10
 cmp dl, 79
@@ -85,11 +94,13 @@ add dl, 1
 mov ah, 0x02
 int 0x10
 jmp Print
-KeyboardFail:
+
+KeyboardFail: ; If keyboard not connected or found, print K to screen meaning Keyboard error, could also be broken connection or other keyboard error
 mov ah, 0x0e
 mov al, 'K'
 int 0x10
-Enter:
+
+Enter: ; Go to start of next line
 mov ah, 0x03
 int 0x10
 cmp dh, 24
@@ -99,5 +110,6 @@ mov dl, 0
 mov ah, 0x02
 int 0x10
 jmp Print
+; End of code
 times 510 - ($ - $$) db 0
 dw 0xaa55
